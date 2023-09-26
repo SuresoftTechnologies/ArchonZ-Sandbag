ac3d9c740968e6277a4c7271a430b6713888d73f
Minhyuk Kwon <minhyuk@suresofttech.com>
2023-09-26 09:24:10 +0900

    modifying overflow condition much lower

--- task/task_overflow_checker.py
+++ task/task_overflow_checker.py
@@ -15,19 +15,20 @@ class Task_Overflow_Checker(engine.Task):
         self.length[msg.arbitration_id] = len(msg.data)
 
         for k, l in self.length.items():   
-            if l > 64:
+            if l > 64 and l <= 70:
                 print(f'{k:02X} is overflow')
                 self._OVERFLOW = True
                 del self.length[msg.arbitration_id]
                 return
         
-        self._OVERFLOW = False
+        
     
     @override
     def get_data(self):
         if not self._OVERFLOW:
             return 0x7AA, [0x01, 0x02, 0x03]  # ok
         else:
+            self._OVERFLOW = False
             return 0xFFF, [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]  # fail
 
 
